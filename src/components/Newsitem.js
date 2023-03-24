43999c310a1d718ae402f4dabb3bc63ca5483c0d
Lovum <sainilovum12@gmail.com>
2023-03-24 22:33:45 +0530

    last commit

---
+++ src/components/Newsitem.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Newsitem = (props) => {
+
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+
+          <img
+            src={
+              !imageUrl
+                ? "https://static.tnn.in/thumb/msid-90497102,imgsize-100,width-1280,height-720,resizemode-75/90497102.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}...<span className="badge bg-secondary">New</span>
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  
+}
+
+export default Newsitem;
